26eacd35979a3a34c82f917a2522f50d605d7b09
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-05-25 16:17:07 +0600

    Initial Commit

---
+++ GeeksProTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeeksProTest
+//
+//  Created by Apple on 25.5.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
